33501ab104e98cc3482029dc46df3aea65ebcb24
chap4ev <sergey.02.nesterenko@gmail.com>
2020-11-22 00:05:30 +0300

    list.h, list.cpp: Added list solution; CMakeLists.txt: Added sanitizers

--- module-1/homework/List/list.h
+++ module-1/homework/List/list.h
@@ -1,8 +1,16 @@
 #pragma once
 #include <cstddef>
-
+#include <iostream>
 
 namespace task {
+
+class Int_node {
+public:
+	int value;
+
+	Int_node* prev;
+	Int_node* next;
+};
 
 
 class list {
@@ -12,7 +20,10 @@ public:
     list();
     list(size_t count, const int& value = int());
 
+    list(const list& other);
+
     ~list();
+
     list& operator=(const list& other);
 
 
@@ -30,22 +41,34 @@ public:
 
     void push_back(const int& value);
     void pop_back();
+
     void push_front(const int& value);
     void pop_front();
+
+
     void resize(size_t count);
+
     void swap(list& other);
 
-
     void remove(const int& value);
-    void unique();
-    void sort();
 
-    // Your code goes here?..
+    void unique();
+
+    void sort();
 
 private:
 
-    // Your code goes here...
+	Int_node* head;
+	Int_node* tail;
+	
+	size_t length;
 
-};
+	void unsafe_delete_node(Int_node* node);
+
+public:
+	
+	void print();
+
+}; // class list
 
 }  // namespace task
